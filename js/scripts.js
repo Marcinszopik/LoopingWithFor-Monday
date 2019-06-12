ff7a2d99f4a9fc732940085d048c0e093a99211c
Marcinszopik <szopiks@yahoo.com>
2019-06-12 07:28:10 -0700

    Add files via upload
    Add files for Monday Homework Looping with for

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+for (var index = 1; index <= 3; index += 1) {
+  alert(index);
+}
+
+var languages = ['HTML', 'CSS', 'Javascript'];
+for (var index = 0; index < languages.length; index += 1) {
+  alert('I love ' + languages[index] + '!');
+}
+
+document.getElementById("demo").innerHTML = text;
